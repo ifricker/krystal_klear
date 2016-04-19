f0c6035a871910a6949a9b343280a71013462881
Alyssa Page & Ian Fricker <pair+ajpage.ifricker@devbootcamp.com>
2016-04-18 18:21:15 -0700

    Add client admin connection

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Rake::Task["db:drop"].invoke
+Rake::Task["db:create"].invoke
+Rake::Task["db:migrate"].invoke
+
+# CREATE ADMIN
+User.create!({
+    name: "admin",
+    email: "admin@krystalklear.com",
+    password: "123456",
+    phone: "123456789",
+    admin: true
+    })
+
+# CREATE STANDARD USERS
+10.times do
+  User.create!({
+    name: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: "123456",
+    phone: "123456789",
+    admin: false
+    })
+end
+
+# CREATE CLIENTS
+50.times do
+  Client.create!({
+    business_name: Faker::Company.name,
+    street_address: Faker::Address.street_address,
+    zip_code: Faker::Address.zip_code,
+    neighborhood: Faker::Address.city,
+    contact_phone: Faker::PhoneNumber.cell_phone,
+    contact_email: Faker::Internet.email
+    })
+
+end
+

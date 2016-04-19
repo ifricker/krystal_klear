963a9a4885a2a6cc012ac01fb9a29a1f60fc6987
Alyssa Page & Ian Fricker <pair+ajpage.ifricker@devbootcamp.com>
2016-04-19 08:51:41 -0700

    Add form container to forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160419015806) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +26,6 @@ ActiveRecord::Schema.define(version: 20160419015806) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
-
 
   create_table "jobs", force: :cascade do |t|
     t.string   "name",            null: false
@@ -46,7 +43,6 @@ ActiveRecord::Schema.define(version: 20160419015806) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-
   end
 
   create_table "users", force: :cascade do |t|

710d3e3b2fc18a9c528c36b112cbfea10797fd02
Alyssa Page & Michael Wang <pair+ajpage.michaelzwang@devbootcamp.com>
2016-04-19 12:29:16 -0700

    Add associations and complete/incomplete work orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419173654) do
+ActiveRecord::Schema.define(version: 20160419183643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,12 +27,18 @@ ActiveRecord::Schema.define(version: 20160419173654) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "employee_routes", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "route_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "jobs", force: :cascade do |t|
     t.string   "name",            null: false
     t.string   "description",     null: false
     t.string   "frequency"
     t.float    "estimated_price", null: false
-    t.float    "final_price"
     t.integer  "client_id",       null: false
     t.integer  "route_id"
     t.datetime "created_at",      null: false
@@ -69,7 +75,7 @@ ActiveRecord::Schema.define(version: 20160419173654) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "work_orders", force: :cascade do |t|
-    t.integer  "employee_id"
+    t.integer  "route_id"
     t.integer  "job_id",                      null: false
     t.float    "final_price"
     t.text     "notes"

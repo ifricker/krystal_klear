a07e0e671820578c0ec539a85730bc1b4b8effd5
Alyssa Page & Ian Fricker <pair+ajpage.ifricker@devbootcamp.com>
2016-04-20 11:31:24 -0700

    Done button for employees

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -2,4 +2,18 @@ class WorkOrder < ActiveRecord::Base
   belongs_to :job
   belongs_to :route
   has_many :employees, through: :route
+
+
+  def incomplete?
+    self.complete == "incomplete"
+  end
+
+  def complete?
+    self.complete == "complete"
+  end
+
+  def passed?
+    self.complete == "pass"
+  end
+
 end
